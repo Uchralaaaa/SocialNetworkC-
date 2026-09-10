9f0a255af4d8805af6b119e932086f4da40a4231
Uchralaaaa <uchka8010@gmail.com>
2026-09-11 01:46:45 +0800

    PostService added

--- SocialPlatform/Repositories/UserRepository.cs
+++ SocialPlatform/Repositories/UserRepository.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
-using System.Linq;
 using SocialPlatform.Interfaces;
 using SocialPlatform.Domain;
 
---
+++ SocialPlatform/Services/PostService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SocialPlatform.Repository;
+using SocialPlatform.Domain;
+using System.Runtime.CompilerServices;
+
+namespace SocialPlatform.Services
+{
+    public class PostService
+    {
+        private readonly PostRepository _postRepository;
+
+        public PostService(PostRepository postRepository)
+        {
+            _postRepository = postRepository;
+        }
+
+        public TextPost CreateTextPost(Guid authorId, string contentText)
+        {
+            var newPost = new TextPost(authorId, contentText);
+            _postRepository.Add(newPost);
+            return newPost;
+        }
+
+        public ImagePost CreateImagePost(Guid authorId, string contentText, string imageURL) 
+        {
+            var newPost = new ImagePost(authorId, contentText)
+            {
+                ImageURL = imageURL
+            };
+            _postRepository.Add(newPost);
+            return newPost;
+        }
+
+        public Post? GetPost(Guid postId)
+        {
+            return _postRepository.GetById(postId);
+        }
+    }
+}
