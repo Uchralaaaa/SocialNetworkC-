2c2562296613073ec40f57fcec739d132daa4575
Uchralaaaa <uchka8010@gmail.com>
2026-09-10 14:43:54 +0800

    UserService added

--- SocialPlatform/Domain/User.cs
+++ SocialPlatform/Domain/User.cs
@@ -11,16 +11,37 @@ namespace SocialPlatform.Domain
         public string UserName { get; set; }
         public DateTime DOB { get; }
         public byte Age => (byte)(DateTime.UtcNow.Year - DOB.Year);
+
+        private string _password = string.Empty;
         public string PassWord 
         {
-            protected get;
-            set 
-            { 
-                if (value == null) {throw new ArgumentNullException("value"); }
-                if (value.Length < 8) { throw new ArgumentException(); }
+            get => _password;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException(nameof(value), "Password cannot be empty!");
+                }
+                if (value.Length < 8)
+                {
+                    throw new ArgumentException("Password must be atleast 8 character!");
+                }
+                _password = value;
             }
         }
 
         public string ProfilePictureURL { get; set; } = "https://example.com/default-avatar.png";
+
+        public User(string username, DateTime dob, string password, string? profilePictureURL)
+        {
+            UserName = username;
+            DOB = dob;
+            _password = password;
+
+            if (!string.IsNullOrWhiteSpace(profilePictureURL))
+            {
+                ProfilePictureURL = profilePictureURL;
+            }
+        }
     }
 }
---
+++ SocialPlatform/Services/UserService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SocialPlatform.Domain;
+
+namespace SocialPlatform.Services
+{
+    public class UserService
+    {
+        private readonly UserRepository _userRepository;
+
+        public UserService(UserRepository userrepository)
+        {
+            _userRepository = userrepository;
+        }
+
+        public User RegisterUser(string username, DateTime DOB, string password, string profilePic)
+        {
+            //user entity uusgeh entity gej yuve 
+            var newUser = new User(username, DOB, password, profilePic);
+            
+            //shine user nemeh
+            _userRepository.Add(newUser);
+
+            return newUser;
+        }
+
+        public User? GetUser(Guid userId) { 
+            return _userRepository.GetById(userId);
+        }
+    }
+}
